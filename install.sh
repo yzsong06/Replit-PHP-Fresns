f6ee2614b33ab7649f6859aedf9dbe9663120f8c
一只鬆 <yzsong06@outlook.com>
2023-07-15 12:20:26 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
 echo Replit_Fresns 安装脚本
 echo Install Fresns To Replit
 echo By The 一只鬆（Yzsong06）
+tag=$(curl -s "https://api.github.com/repos/$repo/releases/latest" | grep -o '"tag_name": ".*"' | sed 's/"tag_name": "//;s/"//')
 nix-env -iA nixpkgs.wget
 wget https://app.fresns.org/latest.zip
 nix-env -iA nixpkgs.unzip
 unzip -d ./fresns latest.zip
+mv ./fresns-$tag.zip fresns
 rm -rf latest.zip
 echo "恭喜搭建完成"
 echo "The setup is complete"
