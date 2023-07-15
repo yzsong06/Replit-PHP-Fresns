a6ae2a682fb6b63ce7c4f9a862a8551c0a7751a4
一只鬆 <yzsong06@outlook.com>
2023-07-15 12:30:25 +0800

    修复解压目录问题

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 echo Replit_Fresns 安装脚本
 echo Install Fresns To Replit
 echo By The 一只鬆（Yzsong06）
+epo="fresns/fresns"
 tag=$(curl -s "https://api.github.com/repos/$repo/releases/latest" | grep -o '"tag_name": ".*"' | sed 's/"tag_name": "//;s/"//')
 nix-env -iA nixpkgs.wget
 wget https://app.fresns.org/latest.zip
